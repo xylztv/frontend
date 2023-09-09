1798e410465778aeb7f4aa14883e3ff162d25eae
xylzlol <xylz2hd@gmail.com>
2023-09-09 20:46:48 +0300

    toastr fix

--- js/config.js
+++ js/config.js
@@ -1,5 +1,7 @@
 export const API_URL = "http://localhost";
-toastr.options = {
-  "closeButton": true, // enables close button
-  "progressBar": true, // enables progress bar
-};
+if (typeof toastr !== 'undefined') {
+  toastr.options = {
+    "closeButton": true, // enables close button
+    "progressBar": true, // enables progress bar
+  };
+}
