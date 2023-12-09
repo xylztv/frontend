24fde2670015984e1a8ee399584b852f28522a6a
xylzlol <xylz2hd@gmail.com>
2023-12-09 22:45:55 +0200

    locally saving list helper data

--- js/listHelpers.js
+++ js/listHelpers.js
@@ -1,26 +1,56 @@
+import { API_URL } from "./config.js";
 
-        // Fetch the helper names from the endpoint
-        import { API_URL } from "./config.js";
-        fetch(`${API_URL}/rest/discord-members`)
-            .then(response => response.json())
-            .then(data => {
-                console.log(data);
-                // Extract the helper names from the response data
-                const helperNames = data.helperMemberData;
-                const adminNames = data.adminMemberData;
+// Function to update the sidebar with helper and admin names
+function updateSidebar(helperNames, adminNames) {
+    const sidebarDescription = document.querySelector('.sidebar-description');
 
-                // Update the sidebar description with the helper names
-                const sidebarDescription = document.querySelector('.sidebar-description');
-                sidebarDescription.innerHTML = helperNames.map(name => `<div class="helper-name">${name}</div>`).join('');
-                // Bold the admin names in the sidebar description
-                adminNames.forEach(name => {
-                    const adminNameElement = document.createElement('div');
-                    adminNameElement.classList.add('helper-name');
-                    adminNameElement.style.fontWeight = 'bold';
-                    adminNameElement.textContent = name;
-                    sidebarDescription.appendChild(adminNameElement);
-                });
-            })
-            .catch(error => {
-                console.error(error);
-            });
+    // Clear existing content
+    sidebarDescription.innerHTML = '';
+
+    // Check if helperNames is defined and is an array
+    if (Array.isArray(helperNames)) {
+        sidebarDescription.innerHTML += helperNames.map(name => `<div class="helper-name">${name}</div>`).join('');
+    }
+
+    // Check if adminNames is defined and is an array
+    if (Array.isArray(adminNames)) {
+        adminNames.forEach(name => {
+            const adminNameElement = document.createElement('div');
+            adminNameElement.classList.add('helper-name');
+            adminNameElement.style.fontWeight = 'bold';
+            adminNameElement.textContent = name;
+            sidebarDescription.appendChild(adminNameElement);
+        });
+    }
+}
+
+
+// Function to fetch helper and admin names from the server
+function fetchDiscordMembers() {
+    fetch(`${API_URL}/rest/discord-members`)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            const helperNames = data.helperMemberData;
+            const adminNames = data.adminMemberData;
+
+            // Update the sidebar with fetched data
+            updateSidebar(helperNames, adminNames);
+
+            // Update local storage with new data
+            localStorage.setItem('discordMembers', JSON.stringify({ helperNames, adminNames }));
+        })
+        .catch(error => {
+            console.error(error);
+        });
+}
+
+// Check local storage first and display data if available
+const storedDiscordMembers = localStorage.getItem('discordMembers');
+if (storedDiscordMembers) {
+    const { helperNames, adminNames } = JSON.parse(storedDiscordMembers);
+    updateSidebar(helperNames, adminNames);
+}
+
+// Fetch the latest data from the server
+fetchDiscordMembers();
